a6d405625a8c40f9c0f673481a9c462e77d7c6b8
ozdmremir <emirhan_512@hotmail.com>
2015-10-10 18:45:27 +0300

    emirhan ozdemir

---
+++ Ekran renklendirme.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+  
+int main()
+{
+    system("color 07");
+    printf("\nEMIRHAN OZDEMIR\n");
+    getch();
+    return 0;
+}
+
